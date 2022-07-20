f933865c5d269c235803ba632de8f78a707583b7
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-20 18:19:55 +0530

    Add files via upload

---
+++ Task23.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class Task23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a;
+System.out.println("The first 10 natural numbers");
+for(a=1;a<10;a++) {
+	System.out.println(a);
+}
+	}
+
+}
