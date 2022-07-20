333dc9edb91e458b862a49607a7ef23d00e4109b
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-20 18:21:44 +0530

    Add files via upload

---
+++ Task27.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,l;
+		System.out.println("Number of Rows");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(i=1;i<n;i++)
+		{
+		for(j=1;j<=n;j++) 
+		System.out.println();
+	}
+
+}
