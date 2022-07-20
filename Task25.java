82b333b9e6f10ae209805c83b857f53cefd560f1
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-20 18:21:05 +0530

    Add files via upload

---
+++ Task25.java
@@ -0,0 +1,27 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task25 {
+
+	public static void main(String[] args) {
+		 int j,n;
+
+		   System.out.print("Input the number(Table to be calculated): ");
+		{
+		   System.out.print("Input number of terms : ");
+		    Scanner sc=new Scanner(System.in);
+		    int n1=sc.nextInt();
+		    
+
+		   System.out.println ("\n");
+		   for(j=0;j<=n1;j++)
+		  
+		     System.out.println(n1+" X "+j+" = " +n1*j);
+		   }
+		}
+	// TODO Auto-generated method stub
+
+	}
+
+
